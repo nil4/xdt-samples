c3c327d5fc6132e6744d3911ce8549a5bd3cf356
nil4 <nil4@users.noreply.github.com>
2016-12-26 00:05:51 +0100

    Add IIS production settings sample skeleton.

---
+++ WebServerSettingsSample/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MvcApp
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("This is a demo of XDT Web.config transformations; nothing to see here!");
+        }
+    }
+}
